25da5102973df47b86c3b77d7acb26f9cef1a26f
derekhsiehdev <devderekhsieh@gmail.com>
2022-12-12 14:49:17 -0800

    finish autocomplete button

--- src/pages/Compose/components/CustomRTE.tsx
+++ src/pages/Compose/components/CustomRTE.tsx
@@ -1,4 +1,4 @@
-import { RichTextEditor, Link } from '@mantine/tiptap';
+import { RichTextEditor, Link, useRichTextEditorContext } from '@mantine/tiptap';
 import { useEditor, BubbleMenu } from '@tiptap/react';
 import Highlight from '@tiptap/extension-highlight';
 import StarterKit from '@tiptap/starter-kit';
@@ -6,7 +6,108 @@ import Underline from '@tiptap/extension-underline';
 import TextAlign from '@tiptap/extension-text-align';
 import Superscript from '@tiptap/extension-superscript';
 import SubScript from '@tiptap/extension-subscript';
+import {IconHighlight, IconDrone} from "@tabler/icons"
 import { Text } from '@mantine/core';
+
+
+function AutocompleteIconButton() {
+  const { editor } = useRichTextEditorContext();
+  return (
+    <RichTextEditor.Control
+      onClick={() => editor?.commands.insertContent('AUTOCOMPLETE')}
+      aria-label="AI autocomplete sentence"
+      title="AI autocomplete"
+    >
+      <IconDrone stroke={1.75} size={18} color={"blue"}/>
+    </RichTextEditor.Control>
+  );
+}
+
+export default function CustomRTE() {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Link,
+      Superscript,
+      SubScript,
+      Highlight,
+      TextAlign.configure({ types: ['heading', 'paragraph'] }),
+    ],
+    content,
+  });
+
+
+  const toolbar = (
+    <RichTextEditor.Toolbar sticky stickyOffset={60}>
+      <RichTextEditor.ControlsGroup>
+        <RichTextEditor.Bold />
+        <RichTextEditor.Italic />
+        <RichTextEditor.Underline />
+        <RichTextEditor.Strikethrough />
+        <RichTextEditor.ClearFormatting />
+      </RichTextEditor.ControlsGroup>
+
+      <RichTextEditor.ControlsGroup>
+        <RichTextEditor.H1 />
+        <RichTextEditor.H2 />
+        <RichTextEditor.H3 />
+        <RichTextEditor.H4 />
+      </RichTextEditor.ControlsGroup>
+
+      <RichTextEditor.ControlsGroup>
+        <RichTextEditor.Blockquote />
+        <RichTextEditor.Hr />
+        <RichTextEditor.BulletList />
+        <RichTextEditor.OrderedList />
+        <RichTextEditor.Subscript />
+        <RichTextEditor.Superscript />
+      </RichTextEditor.ControlsGroup>
+
+      <RichTextEditor.ControlsGroup>
+        <RichTextEditor.Link />
+        <RichTextEditor.Unlink />
+      </RichTextEditor.ControlsGroup>
+
+      <RichTextEditor.ControlsGroup>
+        <RichTextEditor.AlignLeft />
+        <RichTextEditor.AlignCenter />
+        <RichTextEditor.AlignJustify />
+        <RichTextEditor.AlignRight />
+      </RichTextEditor.ControlsGroup>
+
+      <RichTextEditor.ControlsGroup>
+        <AutocompleteIconButton />
+      </RichTextEditor.ControlsGroup>
+    </RichTextEditor.Toolbar>
+
+  )
+
+  return (
+
+    <RichTextEditor editor={editor}>
+      {toolbar}
+
+      {editor && (
+        <BubbleMenu editor={editor}>
+          <RichTextEditor.ControlsGroup>
+            <RichTextEditor.Control
+              onClick={() => editor?.commands.insertContent('⭐')}
+              aria-label="Insert star emoji"
+              title="Insert star emoji"
+            >
+              <Text variant="gradient"
+                gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
+                weight="bold"
+                sx={{ fontFamily: 'Greycliff CF, sans-serif' }} p={5}>rewrite with AI</Text>
+            </RichTextEditor.Control>
+          </RichTextEditor.ControlsGroup>
+        </BubbleMenu>
+      )}
+      <RichTextEditor.Content />
+    </RichTextEditor>
+  );
+}
 
 const content = `
 <p></p>
@@ -45,86 +146,3 @@ const content = `
 <p></p>
 <p></p><p></p>
 `
-
-export default function CustomRTE() {
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Underline,
-      Link,
-      Superscript,
-      SubScript,
-      Highlight,
-      TextAlign.configure({ types: ['heading', 'paragraph'] }),
-    ],
-    content,
-  });
-
-
-  const toolbar = (
-    <RichTextEditor.Toolbar sticky stickyOffset={60}>
-      <RichTextEditor.ControlsGroup>
-        <RichTextEditor.Bold />
-        <RichTextEditor.Italic />
-        <RichTextEditor.Underline />
-        <RichTextEditor.Strikethrough />
-        <RichTextEditor.ClearFormatting />
-        <RichTextEditor.Highlight />
-      </RichTextEditor.ControlsGroup>
-
-      <RichTextEditor.ControlsGroup>
-        <RichTextEditor.H1 />
-        <RichTextEditor.H2 />
-        <RichTextEditor.H3 />
-        <RichTextEditor.H4 />
-      </RichTextEditor.ControlsGroup>
-
-      <RichTextEditor.ControlsGroup>
-        <RichTextEditor.Blockquote />
-        <RichTextEditor.Hr />
-        <RichTextEditor.BulletList />
-        <RichTextEditor.OrderedList />
-        <RichTextEditor.Subscript />
-        <RichTextEditor.Superscript />
-      </RichTextEditor.ControlsGroup>
-
-      <RichTextEditor.ControlsGroup>
-        <RichTextEditor.Link />
-        <RichTextEditor.Unlink />
-      </RichTextEditor.ControlsGroup>
-
-      <RichTextEditor.ControlsGroup>
-        <RichTextEditor.AlignLeft />
-        <RichTextEditor.AlignCenter />
-        <RichTextEditor.AlignJustify />
-        <RichTextEditor.AlignRight />
-      </RichTextEditor.ControlsGroup>
-    </RichTextEditor.Toolbar>
-
-  )
-
-  return (
-
-    <RichTextEditor editor={editor}>
-      {toolbar}
-
-      {editor && (
-        <BubbleMenu editor={editor}>
-          <RichTextEditor.ControlsGroup>
-            <RichTextEditor.Control
-              onClick={() => editor?.commands.insertContent('⭐')}
-              aria-label="Insert star emoji"
-              title="Insert star emoji"
-            >
-              <Text variant="gradient"
-                gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
-                weight="bold"
-                sx={{ fontFamily: 'Greycliff CF, sans-serif' }} p={5}>rewrite with AI</Text>
-            </RichTextEditor.Control>
-          </RichTextEditor.ControlsGroup>
-        </BubbleMenu>
-      )}
-      <RichTextEditor.Content />
-    </RichTextEditor>
-  );
-}
