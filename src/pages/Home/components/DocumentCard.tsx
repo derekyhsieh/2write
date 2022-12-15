aa012e0ddf5154bfa65e237c2912c6bd34409498
aidanbunch <aidanb08@icloud.com>
2022-12-15 09:49:06 -0800

    added upload feature to home page

--- src/pages/Home/components/DocumentCard.tsx
+++ src/pages/Home/components/DocumentCard.tsx
@@ -16,6 +16,8 @@ import {
 	Menu,
 	Center,
 	MediaQuery,
+	Modal,
+	useMantineTheme,
 } from "@mantine/core";
 import {
 	IconDots,
@@ -25,31 +27,38 @@ import {
 	IconNews,
 	IconBuildingArch,
 	IconExclamationMark,
-	IconBulb,
-	TablerIcon,
+	IconUpload,
+	IconCloudUpload,
+	IconX,
+	IconDownload,
 } from "@tabler/icons";
 import React, { useState } from "react";
+import { DropzoneButton } from "./DropzoneButton";
 
 const mockdata = [
 	{
 		title: "Top 10 places to visit in Norway this summer",
 		image:
 			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqwegyXlV-Wy1U1nCq-M02IQkGPo6ki4nxxp__sxKDKNnPjbtqLFSZtAyxqrlzx7lKR74&usqp=CAU",
+		date: "June 12, 2021",
 	},
 	{
 		title: "Best forests to visit in North America",
 		image:
 			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqwegyXlV-Wy1U1nCq-M02IQkGPo6ki4nxxp__sxKDKNnPjbtqLFSZtAyxqrlzx7lKR74&usqp=CAU",
+		date: "January 6, 2021",
 	},
 	{
 		title: "Hawaii beaches review: better than you think",
 		image:
 			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqwegyXlV-Wy1U1nCq-M02IQkGPo6ki4nxxp__sxKDKNnPjbtqLFSZtAyxqrlzx7lKR74&usqp=CAU",
+		date: "December 12, 2020",
 	},
 	{
 		title: "Mountains at night: 12 best locations to enjoy the view",
 		image:
 			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqwegyXlV-Wy1U1nCq-M02IQkGPo6ki4nxxp__sxKDKNnPjbtqLFSZtAyxqrlzx7lKR74&usqp=CAU",
+		date: "November 12, 2020",
 	},
 ];
 
@@ -63,7 +72,7 @@ const useStyles = createStyles((theme) => ({
 		},
 	},
 
-	addNewTemplateCard: {
+	addNewCard: {
 		transition: "transform 150ms ease, box-shadow 150ms ease",
 
 		"&:hover": {
@@ -101,12 +110,18 @@ const useStyles = createStyles((theme) => ({
 	},
 }));
 
+type Props = {
+    setIsActive: (active: boolean) => void;
+ }
+
 interface DocumentCardProps {}
 
 export default function DocumentCard() {
 	const { classes } = useStyles();
 	const [ownerFilter, setOwnerFilter] = useState("Owned by anyone");
 	const [ageFilter, setAgeFilter] = useState("Newest");
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const theme = useMantineTheme()
 
 	const cards = mockdata.map((article) => (
 		<Card
@@ -153,6 +168,7 @@ export default function DocumentCard() {
 			| undefined;
 		templateTitle: string;
 		className: string;
+		onClick?: Function;
 	}) => {
 		return (
 			<Card
@@ -164,6 +180,7 @@ export default function DocumentCard() {
 				withBorder
 				shadow={"sm"}
 				className={props.className}
+				onClick={() => props.onClick ? props.onClick(true) : null}
 			>
 				<Center>
 					<Stack>
@@ -187,92 +204,123 @@ export default function DocumentCard() {
 		);
 	};
 
+	function CreateDropzoneModalContent({setIsActive}: Props) {
+		return (
+				<DropzoneButton onDrop={setIsActive} />
+		)
+	};
+
 	return (
-		<Container fluid className={classes.cardContainer} py={"xl"} px={"5%"}>
-			<Stack spacing={0}>
-				<SimpleGrid
-					cols={5}
-					breakpoints={[{ maxWidth: "sm", cols: 1 }]}
-					mb="xl"
-				>
-					<TemplateCard
-						templateTitle="Add document"
-						className={classes.addNewTemplateCard}
+		<>
+			<Modal
+				opened={modalIsOpen}
+				centered
+				onClose={() => setModalIsOpen(false)}
+				withCloseButton={false}
+				overlayColor={
+					theme.colorScheme === "dark"
+						? theme.colors.dark[9]
+						: theme.colors.gray[2]
+				}
+				overlayOpacity={0.55}
+				overlayBlur={3}
+			>
+				<CreateDropzoneModalContent setIsActive={setModalIsOpen} />
+			</Modal>
+
+			<Container fluid className={classes.cardContainer} py={"xl"} px={"5%"}>
+				<Stack spacing={0}>
+					<SimpleGrid
+						cols={5}
+						breakpoints={[{ maxWidth: "sm", cols: 1 }]}
+						mb="xl"
 					>
-						<IconPlus />
-					</TemplateCard>
-					<TemplateCard
-						templateTitle="Research Essay"
-						className={classes.templateCard}
-					>
-						<IconNews />
-					</TemplateCard>
-					<TemplateCard
-						templateTitle="Historical Essay"
-						className={classes.templateCard}
-					>
-						<IconBuildingArch />
-					</TemplateCard>
-					<TemplateCard
-						templateTitle="Argumentative Essay"
-						className={classes.templateCard}
-					>
-						<IconExclamationMark />
-					</TemplateCard>
-					<TemplateCard
+						<TemplateCard
+							templateTitle="Add document"
+							className={classes.addNewCard}
+						>
+							<IconPlus />
+						</TemplateCard>
+						<TemplateCard
+							templateTitle="Upload document"
+							className={classes.addNewCard}
+							onClick={setModalIsOpen}
+						>
+							<IconUpload />
+						</TemplateCard>
+						<TemplateCard
+							templateTitle="Research Essay"
+							className={classes.templateCard}
+						>
+							<IconNews />
+						</TemplateCard>
+						<TemplateCard
+							templateTitle="Historical Essay"
+							className={classes.templateCard}
+						>
+							<IconBuildingArch />
+						</TemplateCard>
+						<TemplateCard
+							templateTitle="Argumentative Essay"
+							className={classes.templateCard}
+						>
+							<IconExclamationMark />
+						</TemplateCard>
+						{/* <TemplateCard
 						templateTitle="Reflection Essay"
 						className={classes.templateCard}
 					>
 						<IconBulb />
-					</TemplateCard>
-				</SimpleGrid>
-				<Group mb="xl">
-					<Menu transitionDuration={150} transition="rotate-right">
-						<Menu.Target>
-							<Button variant="default" radius="md">
-								<Group position="apart">
-									{ownerFilter} <IconChevronDown />
-								</Group>
-							</Button>
-						</Menu.Target>
-						<Menu.Dropdown>
-							<Menu.Item onClick={() => setOwnerFilter("Owned by anyone")}>
-								Owned by anyone
-							</Menu.Item>
-							<Menu.Item onClick={() => setOwnerFilter("Owned by me")}>
-								Owned by me
-							</Menu.Item>
-							<Menu.Item onClick={() => setOwnerFilter("Not owned by me")}>
-								Not owned by me
-							</Menu.Item>
-						</Menu.Dropdown>
-					</Menu>
-					<Menu transitionDuration={150} transition="rotate-right">
-						<Menu.Target>
-							<Button variant="default" radius="md">
-								<Group position="apart">
-									{ageFilter} <IconChevronDown />
-								</Group>
-							</Button>
-						</Menu.Target>
-						<Menu.Dropdown>
-							<Menu.Item onClick={() => setAgeFilter("Newest")}>
-								Newest
-							</Menu.Item>
-							<Menu.Item onClick={() => setAgeFilter("Oldest")}>
-								Oldest
-							</Menu.Item>
-						</Menu.Dropdown>
-					</Menu>
-				</Group>
-				<SimpleGrid
-					cols={3}
-					breakpoints={[{ maxWidth: "sm", cols: 1 }]}
-					mb="xl"
-				>
-					{cards}
-				</SimpleGrid>
-			</Stack>
-		</Container>
+					</TemplateCard> */}
+					</SimpleGrid>
+					<Group mb="xl">
+						<Menu transitionDuration={150} transition="scale-y">
+							<Menu.Target>
+								<Button variant="default" radius="md">
+									<Group position="apart">
+										{ownerFilter} <IconChevronDown />
+									</Group>
+								</Button>
+							</Menu.Target>
+							<Menu.Dropdown>
+								<Menu.Item onClick={() => setOwnerFilter("Owned by anyone")}>
+									Owned by anyone
+								</Menu.Item>
+								<Menu.Item onClick={() => setOwnerFilter("Owned by me")}>
+									Owned by me
+								</Menu.Item>
+								<Menu.Item onClick={() => setOwnerFilter("Not owned by me")}>
+									Not owned by me
+								</Menu.Item>
+							</Menu.Dropdown>
+						</Menu>
+						<Menu transitionDuration={150} transition="scale-y">
+							<Menu.Target>
+								<Button variant="default" radius="md">
+									<Group position="apart">
+										{ageFilter} <IconChevronDown />
+									</Group>
+								</Button>
+							</Menu.Target>
+							<Menu.Dropdown>
+								<Menu.Item onClick={() => setAgeFilter("Newest")}>
+									Newest
+								</Menu.Item>
+								<Menu.Item onClick={() => setAgeFilter("Oldest")}>
+									Oldest
+								</Menu.Item>
+							</Menu.Dropdown>
+						</Menu>
+					</Group>
+					<SimpleGrid
+						cols={3}
+						breakpoints={[{ maxWidth: "sm", cols: 1 }]}
+						mb="xl"
+					>
+						{cards}
+					</SimpleGrid>
+				</Stack>
+			</Container>
+		</>
 	);
 }
