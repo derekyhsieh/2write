83796a2ddd61ef216444dbff973a4171443f9579
aidanbunch <aidanb08@icloud.com>
2022-12-28 19:43:26 -0800

    search function works, event listener solution commented out

--- src/pages/Home/components/HomeHeader.tsx
+++ src/pages/Home/components/HomeHeader.tsx
@@ -51,7 +51,10 @@ export default function HomeHeader() {
 	useEffect(() => {
 		loadEssayList(user.uid).then((essayList) => {
 			let essaySearch = [];
-			essayList.map((essay) => {
+			let sortedEssayList = essayList.sort(
+				(a, b) => b.lastEdit.toMillis() - a.lastEdit.toMillis()
+			);
+			sortedEssayList.map((essay) => {
 				const essayObject = {};
 				essayObject["value"] = essay.title ? essay.title : "Untitled Document";
 				essayObject["key"] = essay.essayId;
@@ -64,50 +67,50 @@ export default function HomeHeader() {
 		});
 	}, []);
 
-	function SearchComponent(props: {
-		searchQuery;
-		setSearchQuery;
-		essayTitleArray;
-	}) {
-		// Define an event listener callback function
-		const handleKeyDown = useCallback((event) => {
-			setSearchFocus(true);
-			if (event.key === "Enter") {
-				setSearchFocus(false);
-				navigate({
-					pathname: "/",
-					search: `?${createSearchParams({ searchQuery: props.searchQuery })}`,
-				});
-			}
-		}, []);
+	// function SearchComponent(props: {
+	// 	searchQuery;
+	// 	setSearchQuery;
+	// 	essayTitleArray;
+	// }) {
+	// 	// Define an event listener callback function
+	// 	const handleKeyDown = useCallback((event) => {
+	// 		setSearchFocus(true);
+	// 		if (event.key === "Enter") {
+	// 			setSearchFocus(false);
+	// 			navigate({
+	// 				pathname: "/",
+	// 				search: `?${createSearchParams({ searchQuery: props.searchQuery })}`,
+	// 			});
+	// 		}
+	// 	}, []);
 
-		// Add the event listener
-		const ref = useEventListener("keydown", handleKeyDown);
+	// 	// Add the event listener
+	// 	const ref = useEventListener("keydown", handleKeyDown);
 
-		return (
-			<Autocomplete
-				variant="unstyled"
-				autoFocus={searchFocus}
-				ref={ref}
-				value={searchQuery}
-				radius={"md"}
-				onChange={(event: string) => {
-					setSearchQuery(event);
-				}}
-				className={classes.search}
-				placeholder="Search"
-				icon={<IconSearch size={16} stroke={1.5} />}
-				data={essayTitleArray}
-				limit={4}
-				onItemSubmit={(item) => {
-					navigate({
-						pathname: "/compose",
-						search: `?${createSearchParams({ essayId: item.key })}`,
-					});
-				}}
-			/>
-		);
-	}
+	// 	return (
+	// 		<Autocomplete
+	// 			variant="default"
+	// 			autoFocus={searchFocus}
+	// 			ref={ref}
+	// 			value={searchQuery}
+	// 			radius={"md"}
+	// 			onChange={(event: string) => {
+	// 				setSearchQuery(event);
+	// 			}}
+	// 			className={classes.search}
+	// 			placeholder="Search"
+	// 			icon={<IconSearch size={16} stroke={1.5} />}
+	// 			data={essayTitleArray}
+	// 			limit={4}
+	// 			onItemSubmit={(item) => {
+	// 				navigate({
+	// 					pathname: "/compose",
+	// 					search: `?${createSearchParams({ essayId: item.key })}`,
+	// 				});
+	// 			}}
+	// 		/>
+	// 	);
+	// }
 
 	return (
 		<Header height={{ base: 50, md: 70 }} p="md">
@@ -117,10 +120,30 @@ export default function HomeHeader() {
 					<MantineLogo size={28} />
 				</Group>
 
-				<SearchComponent
+				{/* <SearchComponent
 					searchQuery={searchQuery}
 					setSearchQuery={setSearchQuery}
 					essayTitleArray={essayTitleArray}
+				/> */}
+
+				<Autocomplete
+					variant="default"
+					value={searchQuery}
+					radius={"md"}
+					onChange={(event: string) => {
+						setSearchQuery(event);
+					}}
+					className={classes.search}
+					placeholder="Search"
+					icon={<IconSearch size={16} stroke={1.5} />}
+					data={essayTitleArray}
+					limit={4}
+					onItemSubmit={(item) => {
+						navigate({
+							pathname: "/compose",
+							search: `?${createSearchParams({ essayId: item.key })}`,
+						});
+					}}
 				/>
 
 				<Group>
