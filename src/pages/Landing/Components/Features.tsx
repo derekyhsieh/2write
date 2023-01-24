3aa64e6e413f6fcd5d8631e6761e280b0fc8a7cc
derekhsiehdev <devderekhsieh@gmail.com>
2023-01-24 12:43:23 -0800

    add features

---
+++ src/pages/Landing/Components/Features.tsx
@@ -0,0 +1,105 @@
+import {
+    createStyles,
+    Badge,
+    Group,
+    Title,
+    Text,
+    Card,
+    SimpleGrid,
+    Container,
+  } from '@mantine/core';
+  import { IconGauge, IconUser, IconCookie } from '@tabler/icons';
+  
+  const mockdata = [
+    {
+      title: 'AI Autocomplete',
+      description:
+      "Let artificial intelligence convert your ideas into real sentences",
+      icon: IconGauge,
+    },
+    {
+      title: 'Plagarism-free',
+      description:
+      "2write ensures all concent generated is original, checked against Turnitin",
+      icon: IconUser,
+    },
+    {
+      title: 'Outline Assistant',
+      description:
+      'Generate useful and structured essay outlines',
+      icon: IconCookie,
+    },
+  ];
+  
+  const useStyles = createStyles((theme) => ({
+    title: {
+      fontSize: 34,
+      fontWeight: 900,
+      [theme.fn.smallerThan('sm')]: {
+        fontSize: 24,
+      },
+    },
+  
+    description: {
+      maxWidth: 600,
+      margin: 'auto',
+  
+      '&::after': {
+        content: '""',
+        display: 'block',
+        backgroundColor: theme.fn.primaryColor(),
+        width: 45,
+        height: 2,
+        marginTop: theme.spacing.sm,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      },
+    },
+  
+    card: {
+      border: `1px solid ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
+      }`,
+    },
+  
+    cardTitle: {
+      '&::after': {
+        content: '""',
+        display: 'block',
+        backgroundColor: theme.fn.primaryColor(),
+        width: 45,
+        height: 2,
+        marginTop: theme.spacing.sm,
+      },
+    },
+  }));
+  
+  export function Features() {
+    const { classes, theme } = useStyles();
+    const features = mockdata.map((feature) => (
+      <Card key={feature.title} shadow="md" radius="md" className={classes.card} p="xl">
+        <feature.icon size={50} stroke={2} color={theme.fn.primaryColor()} />
+        <Text size="lg" weight={500} className={classes.cardTitle} mt="md">
+          {feature.title}
+        </Text>
+        <Text size="sm" color="dimmed" mt="sm">
+          {feature.description}
+        </Text>
+      </Card>
+    ));
+    return (
+      <Container size="lg" py="xl">
+  
+        <Title order={2} className={classes.title} align="center" mt="sm">
+            Features for your next writing assignment
+        </Title>
+  
+        <Text color="dimmed" className={classes.description} align="center" mt="md">
+        </Text>
+  
+        <SimpleGrid cols={3} spacing="xl" mt={50} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
+          {features}
+        </SimpleGrid>
+      </Container>
+    );
+  }
