9f60c6074347f7906383e41c8247eb6bd903088c
DerekHsiehDev <devderekhsieh@gmail.com>
2023-01-24 20:47:53 -0800

    Revert "Merge pull request #66 from DerekHsiehDev/output"
    This reverts commit c64fd271012551b9b5ad847752d462791c773853, reversing
    changes made to faaf32657414f27c6e94b506e18e5c6fc388fcd6.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import mix, { vercelAdapter } from "vite-plugin-mix";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
-		outDir: path.join(__dirname, "dist"),
 		rollupOptions: {
 		  input: {
 			main: './index.html',
-		  },
+		  }
 		},
 	},
 	plugins: [
