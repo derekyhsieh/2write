2618edbf912aa594297619efbf7154588f1922b3
derekhsiehdev <devderekhsieh@gmail.com>
2023-01-24 20:13:55 -0800

    add adapter and handler

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mix from "vite-plugin-mix";
+import mix, { vercelAdapter } from "vite-plugin-mix";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '',
-	// root: 'src',
-	build: {
-		outDir: '../'
-	},
 	plugins: [
 		react(),
+		//@ts-ignore
 		mix.default({
+			adapter: vercelAdapter,
 			handler: "./handler.ts",
 		}),
 	],
