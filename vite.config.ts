b2aae915f5c42222b5bb95a3df1d1d8afdb5740e
derekhsiehdev <devderekhsieh@gmail.com>
2023-01-26 10:42:25 -0800

    update to vite-plugin-mix

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mix, { nodeAdapter, vercelAdapter } from "vite-plugin-mix";
-import { Adapter } from "vite-plugin-mix";
+import mix, { nodeAdapter, vercelAdapter } from "vite-plugin-mixer";
+import { Adapter } from "vite-plugin-mixer";
 
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"./",
-//   build: {
-//     rollupOptions: {
-// 		output: {
-// 		  assetFileNames: (assetInfo) => {
-// 			var info = assetInfo.name.split(".");
-// 			var extType = info[info.length - 1];
-// 			if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-// 			  extType = "img";
-// 			} else if (/woff|woff2/.test(extType)) {
-// 			  extType = "css";
-// 			}
-// 			return `static/${extType}/[name]-[hash][extname]`;
-// 		  },
-// 		  chunkFileNames: "static/js/[name]-[hash].js",
-// 		  entryFileNames: "static/js/[name]-[hash].js",
-// 		},
-// 	  }
-//   },
+//   base:"./",
+// //   build: {
+// //     rollupOptions: {
+// // 		output: {
+// // 		  assetFileNames: (assetInfo) => {
+// // 			var info = assetInfo.name.split(".");
+// // 			var extType = info[info.length - 1];
+// // 			if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+// // 			  extType = "img";
+// // 			} else if (/woff|woff2/.test(extType)) {
+// // 			  extType = "css";
+// // 			}
+// // 			return `static/${extType}/[name]-[hash][extname]`;
+// // 		  },
+// // 		  chunkFileNames: "static/js/[name]-[hash].js",
+// // 		  entryFileNames: "static/js/[name]-[hash].js",
+// // 		},
+// // 	  }
+// //   },
   plugins: [
     react(),
 
 
 
 
-    //@ts-ignore
-    mix.default({
-
-		
+    mix({
       handler: "./handler.ts",
       adapter: vercelAdapter(),
     }),
