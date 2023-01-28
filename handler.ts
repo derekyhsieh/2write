b6533ce4e46a168dffc62ff70e19a7f4c413792f
DerekHsiehDev <devderekhsieh@gmail.com>
2023-01-27 23:44:53 -0800

    fix import statement

--- handler.ts
+++ handler.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { Configuration, OpenAIApi } from "openai";
-import * as admin from "firebase-admin";
+import admin from "firebase-admin";
 import mongoose from "mongoose";
 
 
