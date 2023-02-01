8b8bb0d9dd7fc05faa9365c2770d7ce2e999a5be
DerekHsiehDev <devderekhsieh@gmail.com>
2023-01-31 20:58:40 -0800

    remove mongodb

--- handler.ts
+++ handler.ts
@@ -44,13 +44,13 @@ app.use("/api", (req, res, next) => {
 	  claims.last_ip = req.socket.remoteAddress;
 	  claims.time_stamp = new Date().toUTCString();
 	  // @ts-ignore
-	  mongoose.connect(uri, function (err, db) {
-		db.collection("User_requests").insertOne(claims, (err, result) => {
-			console.log(result)
-			db.close()
-		})
+	  // mongoose.connect(uri, function (err, db) {
+		// db.collection("User_requests").insertOne(claims, (err, result) => {
+		// 	console.log(result)
+		// 	db.close()
+		// })
 
-	  })
+	  // })
      
 
 
