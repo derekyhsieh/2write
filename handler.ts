48dabc6be57e8b561efc3e0f95bc379343f1edfb
aidanbunch <aidanb08@icloud.com>
2023-01-31 18:34:38 -0800

    changed rewrite prompt to have higher perplexity

--- handler.ts
+++ handler.ts
@@ -71,7 +71,7 @@ app.post("/api/outline", (req, res) => {
 
 app.post("/api/rewrite", (req, res) => {
   getAnswer(
-    "Rewrite the following text in a more professional tone: ".concat(
+    "Rewrite the following text in a more professional tone, with every sentence having a high perplexity over 100: ".concat(
       req.body.prompt
     )
   ).then((answerString) => {
