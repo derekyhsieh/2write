9e4542ae07e8296b33c48529e9226ec3d1e30632
aidanbunch <aidanb08@icloud.com>
2023-02-24 17:47:06 -0800

    fixed bug with updating context and state in essay data drawer, added links for auth page footer

--- src/pages/Compose/components/EssayDataDrawer.tsx
+++ src/pages/Compose/components/EssayDataDrawer.tsx
@@ -1,72 +1,64 @@
-import React, { FC, SetStateAction, useState, useContext, useEffect } from 'react'
-import { CustomDrawer } from './CustomDrawer';
-import { LoginContext } from '../../../context/DocContext';
-import { Textarea, Stack, Center, Title } from '@mantine/core';
-import {  saveEssayPrompt } from '../../../services/FirestoreHelpers';
-import { UserAuth } from '../../../context/AuthContext';
-import { useSearchParams } from 'react-router-dom';
+import React, {
+	FC,
+	SetStateAction,
+	useState,
+	useContext,
+	useEffect,
+} from "react";
+import { CustomDrawer } from "./CustomDrawer";
+import { LoginContext } from "../../../context/DocContext";
+import { Textarea, Stack, Center, Title } from "@mantine/core";
+import { saveEssayPrompt } from "../../../services/FirestoreHelpers";
+import { UserAuth } from "../../../context/AuthContext";
+import { useSearchParams } from "react-router-dom";
 
 interface EssayDataDrawerProps {
-  setOpened: React.Dispatch<SetStateAction<string>>,
-  opened: string,
+	setOpened: React.Dispatch<SetStateAction<string>>;
+	opened: string;
 }
 
 const EssayDataDrawer: FC<EssayDataDrawerProps> = ({ setOpened, opened }) => {
+	// @ts-ignore
+	const { localDocData, setLocalDocData } = useContext(LoginContext);
+	const [searchParams, setSearchParams] = useSearchParams();
 
-  // @ts-ignore
-  const { localDocData, setLocalDocData } = useContext(LoginContext)
-  const [searchParams, setSearchParams] = useSearchParams();
+	const { user } = UserAuth();
 
-  const {user} = UserAuth()
+	const [essayPrompt, setEssayPrompt] = useState("");
 
-  const [essayPrompt, setEssayPrompt] = useState("")
+	useEffect(() => {
+		setEssayPrompt(localDocData.essayPrompt);
+	}, []);
 
+	return (
+		<CustomDrawer setOpened={setOpened} opened={opened == "data"}>
+			<Stack mx={30}>
+				<Center>
+					<Title>Essay Data</Title>
+				</Center>
 
+				<Textarea
+					value={essayPrompt}
+					placeholder="What is the best way to cook pasta?"
+					autosize
+					label="Essay Prompt"
+					onChange={(event) => {
+						saveEssayPrompt(
+							user.uid,
+							searchParams.get("essayId"),
+							event.currentTarget.value
+						);
 
-  useEffect(() => {
+						let newState = localDocData;
 
-
-    setEssayPrompt(localDocData.essayPrompt)
-
-  }, [opened])
-
-  const handleChange = (prompt) => {
-
-    setEssayPrompt(prompt)
-
-    let newState = localDocData
-
-    newState.essayPrompt = prompt
-    setLocalDocData(newState)
-  }
-
-  
-
-  return (
-    <CustomDrawer setOpened={setOpened} opened={opened == "data"}>
-
-      <Stack mx={30}>
-        <Center>
-          <Title>Essay Data</Title>
-        </Center>
-
-        <Textarea
-          value={essayPrompt}
-          placeholder="What is the best way to cook pasta?"
-          autosize
-          label="Essay Prompt"
-        
-          onChange={event => {
-            saveEssayPrompt(user.uid, searchParams.get("essayId"), event.currentTarget.value)
-            setEssayPrompt(event.currentTarget.value)
-          }
-          }
-        />
-
-      </Stack>
-    </CustomDrawer>
-  )
-}
-
+						newState.essayPrompt = event.currentTarget.value;
+						setLocalDocData(newState);
+						setEssayPrompt(event.currentTarget.value);
+					}}
+				/>
+			</Stack>
+		</CustomDrawer>
+	);
+};
 
 export default EssayDataDrawer;
